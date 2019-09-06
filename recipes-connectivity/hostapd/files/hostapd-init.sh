160a54c34780fa06b694bac417d893adbf4040c3
Rajakumaran1106 <rajakumaran.a@ltts.com>
2019-09-06 19:10:52 +0530

    bssid for wlan0 and wlan1 via hostapd
    Signed-off-by: Rajakumaran1106 <rajakumaran.a@ltts.com>

--- recipes-connectivity/hostapd/files/hostapd-init.sh
+++ recipes-connectivity/hostapd/files/hostapd-init.sh
@@ -1,9 +1,17 @@
-!/bin/sh
+#!/bin/sh
 
 if [ ! -f /nvram/hostapd0.conf ]
 then
         cp /etc/hostapd-2G.conf /nvram/hostapd0.conf
         cp /etc/hostapd-5G.conf /nvram/hostapd1.conf
+
+	#Change the bssid for wlan0
+	MAC=`cat /sys/class/net/wlan0/address`
+	sed -i "/^bssid=/c\bssid=${MAC}"  /nvram/hostapd0.conf
+
+	#Change the bssid for wlan1
+	MAC=`cat /sys/class/net/wlan1/address`
+        sed -i "/^bssid=/c\bssid=${MAC}"  /nvram/hostapd1.conf
 fi
 
 brctl addbr brlan0
