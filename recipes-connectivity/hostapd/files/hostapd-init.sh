f41ce641c2249a2378b52f8be07cb9f3c2268af8
manigandan.gopalakrishnan <manigandan.gopalakrishnan@ltts.com>
2019-10-18 21:35:24 +0530

    Virtual Access Points for opensync BackHaul connection
    Signed-off-by: manigandan.gopalakrishnan <manigandan.gopalakrishnan@ltts.com>

--- recipes-connectivity/hostapd/files/hostapd-init.sh
+++ recipes-connectivity/hostapd/files/hostapd-init.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f /nvram/hostapd0.conf ]
 then
-        cp /etc/hostapd-2G.conf /nvram/hostapd0.conf
-        cp /etc/hostapd-5G.conf /nvram/hostapd1.conf
+	cp /etc/hostapd-2G.conf /nvram/hostapd0.conf
+	cp /etc/hostapd-5G.conf /nvram/hostapd1.conf
 
 	#Change the bssid for wlan0
 	MAC=`cat /sys/class/net/wlan0/address`
@@ -11,9 +11,25 @@ then
 
 	#Change the bssid for wlan1
 	MAC=`cat /sys/class/net/wlan1/address`
-        sed -i "/^bssid=/c\bssid=${MAC}"  /nvram/hostapd1.conf
+	sed -i "/^bssid=/c\bssid=${MAC}"  /nvram/hostapd1.conf
 fi
 
-brctl addbr brlan0
-exit 0
+if [ ! -f /sys/class/net/brlan0 ]
+then
+	brctl addbr brlan0
+fi
+ip link set brlan0 address `cat /sys/class/net/eth1/address`
 
+if [ ! -f /sys/class/net/br-home ]
+then
+	brctl addbr br-home
+fi
+ip link set br-home address `cat /sys/class/net/wlan0/address`
+
+MAC=`cat /sys/class/net/wlan0/address`
+iw dev wlan0 interface add wlan12 type __ap addr "00:`echo $MAC | cut -d ':' -f2,3,4,5,6 --output-delimiter=':'`"
+
+MAC=`cat /sys/class/net/wlan1/address`
+iw dev wlan0 interface add wlan13 type __ap addr "00:`echo $MAC | cut -d ':' -f2,3,4,5,6 --output-delimiter=':'`"
+
+exit 0
