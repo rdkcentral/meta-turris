e1ab2a6f258502071f6b7402a1c5e8297df97e56
viswa.g <viswa.g@ltts.com>
2023-08-08 16:58:00 +0530

    REFPLTB-2629 : Turris GW: NAT is not working with q2 build
    Reason for change : Connected clients are not getting Internet while connected with Turris GW
    Test Procedure: Lan clients and wifi clients are getting Internet
    Risks: Low
    
    Signed-off-by: viswa.g <viswa.g@ltts.com>

--- recipes-ccsp/ccsp/files/udhcpc_check.sh
+++ recipes-ccsp/ccsp/files/udhcpc_check.sh
@@ -1,8 +1,11 @@
 #!bin/sh
 #### Added workaround fix for getting erouter0 IP
-sleep 60
+sleep 30
 EROUTER_IP=`ifconfig erouter0 | grep "inet addr" | xargs | cut -d ':' -f2 | cut -d ' ' -f1`
 echo "erouter0 ip is $EROUTER_IP"
 if [ "$EROUTER_IP" = "" ] ; then  
      udhcpc -i erouter0
 fi
+EROUTER_IP=`ifconfig erouter0 | grep "inet addr" | xargs | cut -d ':' -f2 | cut -d ' ' -f1`
+sysevent set current_wan_ipaddr $EROUTER_IP
+
